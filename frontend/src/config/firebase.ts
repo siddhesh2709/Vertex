65ccec6a15d23c7be877c04da525c2fbdc2ba640
Siddhesh <siddhesh111avhad@gmail.com>
2026-09-17 19:08:57 +0530

    Resolve merge conflicts from stash pop

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -4,9 +4,9 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBZfTeo7qZeQiL-1VWXdchC6Gtf3ho-7TI",
-    authDomain: "agrolyft.firebaseapp.com",
-    projectId: "agrolyft",
-    storageBucket: "agrolyft.firebasestorage.app",
+    authDomain: "Vertex.firebaseapp.com",
+    projectId: "Vertex",
+    storageBucket: "Vertex.firebasestorage.app",
     messagingSenderId: "504889344388",
     appId: "1:504889344388:web:cac7df17ab76f0fbd09dd1",
     measurementId: "G-PEMK08NEJC"
